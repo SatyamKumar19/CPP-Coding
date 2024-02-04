a1a7a79057b0e41accc2a0a169c3698dc2b3bb05
SatyamKumar19 <158846498+SatyamKumar19@users.noreply.github.com>
2024-02-04 23:40:28 +0530

    Basics

---
+++ practices2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){/*
+double a=2.5/7;
+cout<<a<<endl;
+return 0;*/
+//int a=5;
+/*int b=5;
+bool first=(a==b);
+cout<<first<<endl;
+bool second=(a>b);
+cout<<second<<endl;
+bool third=(a<b);
+cout<<third<<endl;
+bool fourth=(a<=b);
+cout<<fourth<<endl;
+bool fifth=(a!=b);
+cout<<fifth<<endl;
+bool sixth=(a>=b);
+cout<<sixth<<endl;*/
+/*int a=0;
+cout<<!a<<endl;*/
+int a=77||44;
+cout<<a<<endl;
+}
