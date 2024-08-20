e0cb374d317c9edb018135a4d4a1b5576753b33c
SatyamKumar19 <158846498+SatyamKumar19@users.noreply.github.com>
2024-08-20 22:24:27 +0530

    first

---
+++ hello.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class hero {
+
+public :
+ int health;
+ char level;
+
+
+
+
+};
+int main(){
+
+hero ramesh;
+ramesh.health=11;
+ ramesh.level='s';
+ cout<<"health is "<<ramesh.health<<endl;
+ cout<<"level is "<<ramesh.level<<endl;
+
+
+
+}
