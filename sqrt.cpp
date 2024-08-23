d5a88f961b7c06f9f53fd085193c70374f1f4291
SatyamKumar19 <158846498+SatyamKumar19@users.noreply.github.com>
2024-08-23 22:50:53 +0530

    Add files via upload

---
+++ sqrt.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+int a;
+cout<<"enter the number"<<endl;
+cin>>a;
+int root=sqrt(a);
+cout<<"the root"<< a<< " is "<<root<<endl;
+return 0;
+
+
+}
